aea800a96c7d3dbdfa7bee732014c70d0148984c
A. K. M. NAZMUL ISLAM <nazmulislamredoy@gmail.com>
2017-07-10 18:12:06 +0600

    updated

---
+++ js/custom.js
@@ -0,0 +1,8 @@
+$(document).ready(function () {
+    $("#work-exp-form-cancel").click(function () {
+        $("#add-experience-content").hide();
+    });
+    $("#add-experience").click(function () {
+        $("#add-experience-content").show();
+    });
+});
